f35ef7e6e17e8609fd3a27c90a536e0b4d6bb23b
shruti sonawane <169151468+Shrutie18@users.noreply.github.com>
2024-11-06 10:51:24 +0530

    save

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "this_is_ubuntu" {
+
+    ami = "ami-0e0e417dfa2028266 " 
+    disable_api_stop  = false 
+    disable_api_termination = false  
+    instance_type = "t2.micro" 
+    key_name= "shrutikey"
+    vpc_security_group_ids = ["sg-08e875220cb8c5d83"]
+    count = 4  #loop 
+    tags = {
+      purpose = "webserver"
+    } 
+    
+
+}    
