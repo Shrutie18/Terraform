89dccd6fe79d89832f34351fc9edb1735346f26b
shruti sonawane <169151468+Shrutie18@users.noreply.github.com>
2024-11-08 15:10:56 +0530

    qwe

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -3,7 +3,7 @@ output "main_route_table_id_vpc_one" {
 }
 
 output "vpc_cidr_vpc_one" {
-  value = module.vpc_one.vpc_cidr
+  value = module.vpc_one.cidr_block
 }
 
 output "main_route_table_id_vpc_two" {
@@ -11,5 +11,5 @@ output "main_route_table_id_vpc_two" {
 }
 
 output "vpc_cidr_vpc_two" {
-  value = module.vpc_two.vpc_cidr
+  value = module.vpc_two.cidr_block
 }
--- vpc/routes.tf
+++ vpc/routes.tf
@@ -1,9 +1,9 @@
 resource "aws_route" "route_to_peer_vpc_one" {
   route_table_id            = module.vpc_one.main_route_table_id
-  destination_cidr_block    = module.vpc_two.vpc_cidr
+  destination_cidr_block    = module.vpc_two.cidr_block
 }
 
 resource "aws_route" "route_to_peer_vpc_two" {
   route_table_id            = module.vpc_two.main_route_table_id
-  destination_cidr_block    = module.vpc_one.vpc_cidr
+  destination_cidr_block    = module.vpc_one.cidr_block
 }
