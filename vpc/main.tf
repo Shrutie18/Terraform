523fbda8532843852d6a20bbf1f76005502d9035
shruti sonawane <169151468+Shrutie18@users.noreply.github.com>
2024-11-08 16:40:09 +0530

    zsexdf

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,21 +1,54 @@
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_support = true
-  enable_dns_hostnames = true
-}
-module "vpc_one" {
-  source = "terraform-aws-modules/vpc/aws"
-  name   = "vpc-one"
-  cidr   = var.vpc_one_cidr
-  enable_dns_support = true
-  enable_dns_hostnames = true
+# Creating T2S VPC Dev
+resource "aws_vpc" "t2s-vpc-dev" {
+  cidr_block = var.t2s-vpc-dev-cidr
+
+  tags = {
+    Name = "t2s-vpc-dev"
+  }
 }
 
-module "vpc_two" {
-  source = "terraform-aws-modules/vpc/aws"
-  name   = "vpc-two"
-  cidr   = var.vpc_two_cidr
-  enable_dns_support = true
-  enable_dns_hostnames = true
+# Creating T2S VPC Stage
+resource "aws_vpc" "t2s-vpc-stage" {
+  cidr_block = var.t2s-vpc-stage-cidr
+
+  tags = {
+    Name = "t2s-vpc-stage"
+  }
 }
 
+data "aws_availability_zones" "available_zones" {
+  state = "available"
+}
+
+# Creating Subnet Dev in T2S VPC Dev
+resource "aws_subnet" "subnet-dev" {
+  vpc_id            = aws_vpc.t2s-vpc-dev.id
+  cidr_block        = var.subnet-dev-cidr
+  availability_zone = data.aws_availability_zones.available_zones.names[0]
+
+  tags = {
+    Name = "Dev-Subnet"
+  }
+}
+
+# Creating Subnet Stage in T2S VPC Stage
+resource "aws_subnet" "subnet-stage" {
+  vpc_id            = aws_vpc.t2s-vpc-stage.id
+  cidr_block        = var.subnet-stage-cidr
+  availability_zone = data.aws_availability_zones.available_zones.names[1]
+
+  tags = {
+    Name = "Stage-Subnet"
+  }
+}
+
+# Creating and Configuring the Peering connection.
+resource "aws_vpc_peering_connection" "vpc_peering" {
+  vpc_id      = aws_vpc.t2s-vpc-dev.id
+  peer_vpc_id = aws_vpc.t2s-vpc-stage.id
+}
+
+resource "aws_vpc_peering_connection_accepter" "vpc_peering_accepter" {
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peering.id
+  auto_accept               = true
+}
