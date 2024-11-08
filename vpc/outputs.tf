ce49768a8a611dcb9699f10466e03897c4947d9d
shruti sonawane <169151468+Shrutie18@users.noreply.github.com>
2024-11-08 14:44:56 +0530

    Update outputs.tf

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,10 +1,4 @@
-output "vpc_one_peering_connection_id" {
-  value = aws_vpc_peering_connection.peer.id
-}
 
-output "vpc_two_peering_connection_id" {
-  value = aws_vpc_peering_connection.peer.id
-}
 output "main_route_table_id" {
   value = aws_vpc.main_route_table_id
 }
