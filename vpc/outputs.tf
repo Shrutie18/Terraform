72c7e61eb470b8710f955bb96a5d5c081f840905
shruti sonawane <169151468+Shrutie18@users.noreply.github.com>
2024-11-08 15:21:39 +0530

    dfv

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -1,15 +1,15 @@
-#output "main_route_table_id_vpc_one" {
-  #value = module.vpc_one.main_route_table_id
+output "main_route_table_id_vpc_one" {
+  value = module.vpc_one.route_table_ids[0]
 }
 
 output "vpc_cidr_vpc_one" {
-  value = module.vpc_one.vpc_cidr
+  value = module.vpc_one.cidr_block
 }
 
 output "main_route_table_id_vpc_two" {
-  value = module.vpc_two.main_route_table_id
+  value = module.vpc_two.route_table_ids[0]
 }
 
 output "vpc_cidr_vpc_two" {
-  value = module.vpc_two.vpc_cidr
+  value = module.vpc_two.cidr_block
 }
