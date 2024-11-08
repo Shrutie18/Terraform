94354934a3065956108e84e3247591e0ce7a73eb
shruti sonawane <169151468+Shrutie18@users.noreply.github.com>
2024-11-08 16:45:19 +0530

    zsxd

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,14 +1,5 @@
+# Inputting the Region Where To Deploy. 
 variable "region" {
-  description = "The AWS region to deploy resources"
-  default     = "us-west-2"
-}
-
-variable "vpc_one_cidr" {
-  description = "CIDR block for VPC one"
-  default     = "10.0.0.0/16"
-}
-
-variable "vpc_two_cidr" {
-  description = "CIDR block for VPC two"
-  default     = "10.1.0.0/16"
+  default = "us-east-1"
+  description = "The Region where to deploy"
 }
