f8026c94018e764e924c9d1d30bcf1e8002dd4ba
shruti sonawane <169151468+Shrutie18@users.noreply.github.com>
2024-11-08 12:50:52 +0530

    Update main.tf

--- EC2/main.tf
+++ EC2/main.tf
@@ -57,3 +57,36 @@ resource "aws_instance" "example" {
     Name = "example-instance"
   }
 }
+# Internet Gateway
+resource "aws_internet_gateway" "example_igw" {
+  vpc_id = aws_vpc.example_vpc.id
+  tags = {
+    Name = "example-igw"
+  }
+}
+
+# Route Table
+resource "aws_route_table" "example_route_table" {
+  vpc_id = aws_vpc.example_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.example_igw.id
+  }
+
+  tags = {
+    Name = "example-route-table"
+  }
+}
+
+# Associate Route Table with Subnet
+resource "aws_route_table_association" "example_association" {
+  subnet_id      = aws_subnet.example_subnet.id
+  route_table_id = aws_route_table.example_route_table.id
+}
+
+# Elastic IP for EC2 Instance
+resource "aws_eip" "example_eip" {
+  instance = aws_instance.example.id
+}
+
