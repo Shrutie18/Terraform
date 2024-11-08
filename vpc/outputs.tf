5e7e53f7b4c9f607e000dcce68c29070a95c1c3e
shruti sonawane <169151468+Shrutie18@users.noreply.github.com>
2024-11-08 14:43:13 +0530

    Update outputs.tf

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -5,3 +5,11 @@ output "vpc_one_peering_connection_id" {
 output "vpc_two_peering_connection_id" {
   value = aws_vpc_peering_connection.peer.id
 }
+output "main_route_table_id" {
+  value = aws_vpc.main_route_table_id
+}
+
+output "vpc_cidr" {
+  value = aws_vpc.cidr_block
+}
+
