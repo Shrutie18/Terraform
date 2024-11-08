5ed2b59f3622587cabe1eef28d6a65ae80da0893
shruti sonawane <169151468+Shrutie18@users.noreply.github.com>
2024-11-08 15:06:15 +0530

    sdx

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,20 +1,32 @@
-# This file should only contain resource definitions, routes, etc.
-# No module calls for vpc_one or vpc_two here.
-
-resource "aws_vpc" "vpc_one" {
-  cidr_block = var.vpc_one_cidr
+module "vpc_one" {
+  source             = "terraform-aws-modules/vpc/aws"
+  name               = "vpc-one"
+  cidr               = var.vpc_one_cidr
   enable_dns_support = true
   enable_dns_hostnames = true
-  tags = {
-    Name = "vpc-one"
-  }
 }
 
-resource "aws_vpc" "vpc_two" {
-  cidr_block = var.vpc_two_cidr
+module "vpc_two" {
+  source             = "terraform-aws-modules/vpc/aws"
+  name               = "vpc-two"
+  cidr               = var.vpc_two_cidr
   enable_dns_support = true
   enable_dns_hostnames = true
-  tags = {
-    Name = "vpc-two"
-  }
+}
+
+# Add the following outputs for the attributes you want to access
+output "vpc_one_main_route_table_id" {
+  value = module.vpc_one.main_route_table_id
+}
+
+output "vpc_one_cidr" {
+  value = module.vpc_one.vpc_cidr
+}
+
+output "vpc_two_main_route_table_id" {
+  value = module.vpc_two.main_route_table_id
+}
+
+output "vpc_two_cidr" {
+  value = module.vpc_two.vpc_cidr
 }
