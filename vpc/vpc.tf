b71d99803b3a967d470316b393ff93682e486231
shruti sonawane <169151468+Shrutie18@users.noreply.github.com>
2024-11-08 15:08:29 +0530

    x

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,32 +1,16 @@
-module "vpc_one" {
-  source             = "terraform-aws-modules/vpc/aws"
-  name               = "vpc-one"
-  cidr               = var.vpc_one_cidr
-  enable_dns_support = true
-  enable_dns_hostnames = true
-}
-
-module "vpc_two" {
-  source             = "terraform-aws-modules/vpc/aws"
-  name               = "vpc-two"
-  cidr               = var.vpc_two_cidr
-  enable_dns_support = true
-  enable_dns_hostnames = true
-}
-
-# Add the following outputs for the attributes you want to access
-output "vpc_one_main_route_table_id" {
-  value = module.vpc_one.main_route_table_id
-}
-
-output "vpc_one_cidr" {
-  value = module.vpc_one.vpc_cidr
-}
-
-output "vpc_two_main_route_table_id" {
-  value = module.vpc_two.main_route_table_id
-}
-
-output "vpc_two_cidr" {
-  value = module.vpc_two.vpc_cidr
-}
+# Remove these blocks from vpc.tf to avoid duplicates
+# module "vpc_one" {
+#   source             = "terraform-aws-modules/vpc/aws"
+#   name               = "vpc-one"
+#   cidr               = var.vpc_one_cidr
+#   enable_dns_support = true
+#   enable_dns_hostnames = true
+# }
+#
+# module "vpc_two" {
+#   source             = "terraform-aws-modules/vpc/aws"
+#   name               = "vpc-two"
+#   cidr               = var.vpc_two_cidr
+#   enable_dns_support = true
+#   enable_dns_hostnames = true
+# }
