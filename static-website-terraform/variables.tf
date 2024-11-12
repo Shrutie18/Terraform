f5e71c083bfc136b82fc3f605c282ad54cff9664
shruti sonawane <169151468+Shrutie18@users.noreply.github.com>
2024-11-12 13:44:35 +0530

    zsdx

--- static-website-terraform/variables.tf
+++ static-website-terraform/variables.tf
@@ -1,31 +1,24 @@
-# variables.tf
-
-# AWS Region (default: us-east-1)
 variable "region" {
-  description = "AWS Region"
-  default     = "us-east-1"
+  description = "AWS region"
+  type        = string
 }
 
-# EC2 Instance Type (default: t2.micro)
 variable "instance_type" {
   description = "EC2 instance type"
-  default     = "t2.micro"
+  type        = string
 }
 
-# AMI ID for Ubuntu (replace with your preferred AMI)
 variable "ami_id" {
-  description = "AMI ID to use for EC2 instance"
-  default     = "ami-005fc0f236362e99f"  # Ubuntu AMI ID
+  description = "AMI ID"
+  type        = string
 }
 
-# Key Name for EC2 access
 variable "key_name" {
-  description = "SSH key name to access EC2 instance"
-  default     = "shruti1"
+  description = "Key pair name"
+  type        = string
 }
 
-# Domain name for Route 53
 variable "domain_name" {
-  description = "Domain name for Route 53"
-  default     = "shrutee.site"
+  description = "Domain name for the website"
+  type        = string
 }
