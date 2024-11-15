31856c872dcd326fb864ee041e9ae39aeb428442
shruti sonawane <169151468+Shrutie18@users.noreply.github.com>
2024-11-15 11:21:34 +0530

    wze

---
+++ outputblock/variable.tf
@@ -0,0 +1,64 @@
+variable "this_image_id" {
+  type        = string
+  default = "ami-012967cc5a8c9f891 "
+}
+
+variable "this_disable_api_stop" {
+  type        = bool
+  default = false
+}
+variable "this_disable_api_termination" {
+    type = bool 
+    default = false
+    description = "this variable is used to pass bool data to api_termination" 
+}
+variable "this_count" {
+    type = number 
+    default = 2
+     
+}
+variable "this_vpc_security_group_ids" {
+    type = string 
+    default = "sg-01c97071bf05b4c77"
+     
+}
+
+variable "this_list" {
+    type = list 
+    default = ["t2.micro" , "2" , "false"]
+     
+}
+
+variable "this_map" {
+    type = map  
+    default = {
+     purposeec2 = "webserver"   
+     termination = true 
+     count = 1
+
+    }
+     
+}
+
+#vpc variables starts from here 
+variable "this_vpc_cidr_block" {
+    type = string
+    default = "172.31.0.0/16"
+}
+variable "this_vpc_tags" {
+     type = string
+     default = "this_vpc"
+}
+variable "this_subnet_pub_cidr_block" {
+     type = string
+     default = "172.31.16.0/20"
+}
+
+variable "this_subnet_pub_map_ip" {
+    type = bool
+    default = true
+}
+variable "this_subnet_pub_tags" {
+    type = string
+    default = "pub_subnet"
+}
