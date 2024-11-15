557952b2120db14433b31b94537f48ab822aa51f
shruti sonawane <169151468+Shrutie18@users.noreply.github.com>
2024-11-15 17:24:50 +0530

    Update output.tf

--- studentapplication/output.tf
+++ studentapplication/output.tf
@@ -1,10 +1,10 @@
 # Output the Public ip of the EC2 instance
 output "public_ip" {
   description = "The public IP address of the EC2 instance."
-  value       = aws_instance.studentapp.public_ip
+  value       = aws_instance.StudentApp.public_ip
 }
 
 # Output the Public DNS of the EC2 instance
 output "public_dns" {
-  value = aws_instance.studentapp.public_dns
+  value = aws_instance.StudentApp.public_dns
 }
