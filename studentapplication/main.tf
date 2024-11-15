10426e3f0d6568d3ef79ca612071d70e0bfe4f81
shruti sonawane <169151468+Shrutie18@users.noreply.github.com>
2024-11-15 17:59:40 +0530

    zsdx

--- studentapplication/main.tf
+++ studentapplication/main.tf
@@ -1,67 +1,6 @@
-provider "aws" {
-  region = var.this_aws_region
-}
-
-# Create VPC
-resource "aws_vpc" "studentvpc" {
-  cidr_block = var.this_vpc_cidr_block
-  enable_dns_support = true
-  enable_dns_hostnames = true
-  instance_tenancy = "default"
-  tags = {
-    Name = var.this_vpc_name
-  }
-}
-
-# Create Public Subnet in the VPC
-resource "aws_subnet" "studentsubnet" {
-  vpc_id            = aws_vpc.studentvpc.id
-  cidr_block        = var.this_subnet_cidr_block
-  availability_zone = var.this_availability_zone
-  map_public_ip_on_launch = true
-  tags = {
-    Name = var.this_subnet_name
-  }
-}
-
-# Create Internet Gateway for internet access
-resource "aws_internet_gateway" "studentigw" {
-  vpc_id = aws_vpc.studentvpc.id
-  tags = {
-    Name = var.this_internet_gateway_name
-  }
-}
-
-# Create Route Table
-resource "aws_route_table" "studentrt" {
-  vpc_id = aws_vpc.studentvpc.id
-  tags = {
-    Name = "studentrt"
-  }
-}  
-
-resource "aws_route" "publicroute" {
-  route_table_id = aws_route_table.studentrt.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.studentigw.id
-}
- 
-# Associate Route Table with Subnet
-resource "aws_route_table_association" "studentsa" {
-  subnet_id      = aws_subnet.studentsubnet.id
-  route_table_id = aws_route_table.studentrt.id
-}
-
-# Create Security Group within the VPC
-resource "aws_security_group" "studentsecuritygroup" {
-  vpc_id = aws_vpc.studentvpc.id  # Ensure it is in the correct VPC
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+# Security Group for EC2 Instance (Tomcat Web Server)
+resource "aws_security_group" "web_server" {
+  #vpc_id = aws_vpc.main_vpc.id
 
   ingress {
     from_port   = 80
@@ -69,10 +8,15 @@ resource "aws_security_group" "studentsecuritygroup" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   ingress {
     from_port   = 8080
     to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -85,39 +29,33 @@ resource "aws_security_group" "studentsecuritygroup" {
   }
 
   tags = {
-    Name = var.this_security_group_name
+    Name = "StudentApp-SG"
   }
 }
 
-# EC2 Instance Creation
-resource "aws_instance" "StudentApp" {
-  ami                          = var.this_ami
-  instance_type                = var.this_instance_type
-  associate_public_ip_address  = var.this_associate_public_ip
-  subnet_id                    = aws_subnet.studentsubnet.id
-  vpc_security_group_ids       = [aws_security_group.studentsecuritygroup.id]  # Use vpc_security_group_ids
-  key_name                     = var.this_key_name 
-  tags = {
-    Name = var.this_instance_name
-  }
+# EC2 Instance for Web Server (Student App with Tomcat 9)
+resource "aws_instance" "web_server_instance" {
+  ami                    = var.ubuntu_ami
+  instance_type          = var.aws_instance_type
+  security_groups        = [aws_security_group.web_server.name]
+  key_name               = var.key_name
+  associate_public_ip_address = true
 
   user_data = <<-EOF
               #!/bin/bash
               sudo apt update -y
-              sudo apt install openjdk-11-jre-headless -y
-              sudo apt install maven -y
+              sudo apt install openjdk-11-jre-headless -y maven
               sudo apt install mariadb-client -y
 
               # Clone the repository
               git clone https://github.com/Aamantamboli/Studentapp.git /home/ubuntu/Studentapp
-
               # Navigate to the project directory and build it
               cd /home/ubuntu/Studentapp
               mvn clean package
 
               # Download and install Tomcat
               cd /tmp
-              wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.97/bin/apache-tomcat-9.0.97.tar.gz
+              wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.97/bin/apache-tomcat-9.0.97.tar.gz  #Check for new version if doesnt work
               tar -xvf apache-tomcat-9.0.97.tar.gz
               sudo mv apache-tomcat-9.0.97 /opt/tomcat
 
@@ -131,5 +69,9 @@ resource "aws_instance" "StudentApp" {
 
               # Start Tomcat
               sudo bash /opt/tomcat/bin/catalina.sh start
-    EOF
+              EOF
+
+  tags = {
+    Name = "StudentApp"
+  }
 }
---
+++ studentapplication/terraform.auto.tfvars
@@ -0,0 +1,3 @@
+ubuntu_ami        = "ami-0866a3c8686eaeeba"  # Use an appropriate AMI ID for your region
+aws_instance_type = "t2.micro"
+key_name          = "shruti1"     # Replace with your EC2 key pair name
