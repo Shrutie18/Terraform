e831b747e171059284ad11ea79c75c7fdb77acc7
shruti sonawane <169151468+Shrutie18@users.noreply.github.com>
2024-11-15 11:48:10 +0530

    Update main.tf

--- outputblock/main.tf
+++ outputblock/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "this_ubuntu" {
     disable_api_stop  = var.this_disable_api_stop    #var.this_any.api_stop_ec2   
     disable_api_termination =  var.this_disable_api_termination   #var.this_any.api_termination_ec2 
     instance_type = var.this_list[0]  #var.this_any.instance_type_list[0]
-    key_name="batmobile"
+    key_name= "nvirginia"
     #vpc_security_group_ids = [var.this_vpc_security_group_ids]
       depends_on = [aws_subnet.this_subnet_pub]
     subnet_id =  aws_subnet.this_subnet_pub.id
