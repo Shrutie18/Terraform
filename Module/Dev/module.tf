f54d3b8347389b4d9f6d402c1e8961a9dddb51f4
shruti sonawane <169151468+Shrutie18@users.noreply.github.com>
2024-11-21 11:06:24 +0530

    ts

--- Module/Dev/module.tf
+++ Module/Dev/module.tf
@@ -1,21 +1,21 @@
 
 module "ec2" {
-    source = "/home/anup/terraform/deleteafterpractice/Module/Resources/EC2"
-    this_image_id = "ami-012967cc5a8c9f891"
+    source = "/home/cloudshell-user/Terraform/Module/Resources/EC2"
+    this_image_id = "ami-005fc0f236362e99f"
     this_list = "t2.micro"
     this_disable_api_stop = false
     this_disable_api_termination = false
-    #this_vpc_security_group_ids = "sg-0505874879e7ce6cd"
+    #this_vpc_security_group_ids = "sg-01c97071bf05b4c77"
     this_aws_instance_subnet = module.vpc.subnet_id
     #this_sg_vpc_id = module.vpc.vpc_id
     //this_aws_vpc_id = module.aws_vpc.aws_vpc_id
 }
 
 module "vpc" {
-     source = "/home/anup/terraform/deleteafterpractice/Module/Resources/VPC"
-     this_vpc_cidr_block = "12.11.0.0/16"
+     source = "/home/cloudshell-user/Terraform/Module/Resources/Vpc"
+     this_vpc_cidr_block = "172.31.0.0/16"
      this_vpc_tags = "this_vpc"
-     this_subnet_pub_cidr_block = "12.11.0.0/17"
+     this_subnet_pub_cidr_block = "172.31.16.0/20"
      this_subnet_pub_map_ip  = true 
      this_subnet_pub_tags = "pub_subnet"
      this_vpc_az = "us-east-1a"
--- Module/Resources/EC2/variable.tf
+++ Module/Resources/EC2/variable.tf
@@ -10,7 +10,7 @@ variable "this_count" {
 }
 variable "this_vpc_security_group_ids" {
     type = string 
-    default = "sg-0505874879e7ce6cd"
+    default = "sg-01c97071bf05b4c77"
      
 }
 
