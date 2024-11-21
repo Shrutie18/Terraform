0df5e796005bc38de121b9d10dda5c0c737285e3
shruti sonawane <169151468+Shrutie18@users.noreply.github.com>
2024-11-21 16:36:16 +0530

    xd

--- terraform-s3-website/outputs.tf
+++ terraform-s3-website/outputs.tf
@@ -1,4 +1,4 @@
 output "website_url" {
   description = "The URL of the static website"
-  value       = aws_s3_bucket.website.website_endpoint
+  value       = "http://${aws_s3_bucket.website.bucket}.s3-website.${var.region}.amazonaws.com"
 }
