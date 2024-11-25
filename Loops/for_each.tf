a000eac2a14b7205a4aab2790e999c6d13a1d34d
shruti sonawane <169151468+Shrutie18@users.noreply.github.com>
2024-11-25 11:32:00 +0530

    dxc

--- Loops/for_each.tf
+++ Loops/for_each.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_user" "main_user1" {
     name =  var.aws_user_name-1[count.index]
-    count = length(var.aws_user_name)
+    count = length(var.aws_user_name-1)
 }
 
  variable "aws_user_name-1" {
